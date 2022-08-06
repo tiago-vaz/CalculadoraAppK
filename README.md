c55fa287b879af9172102efa942050208b1b72a3
Tiago Vaz <tiago-vaz@hotmail.com>
2022-08-06 12:24:18 -0300

    Primeira versão

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CalculadoraAppK
  
+versionCode = 0.1
+
+versionName = "0.1"
+
+
+# Descrição do projeto
+Aplicativo exemplo utilizado como introdução a Android com Kotlin.
+
+Este aplicativo calcula qual tipo de combustível é mais interessante de utilizar no abastecimento, baseado nos valores fornecidos para Álcool e Gasolina
+
+O cálculo é feito da seguinte maneira:
+
+```
+Coeficiente = Preço do Álcool / Preço da Gasolina
+
+Se o coeficiente for maior que 0.7, o app informa para utilizar Gasolina
+
+Se o coeficiente for menor que 0.7, o app informa para utilizar Álcool
+```
--- app/src/main/java/com/example/calculadoraappk/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraappk/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.calculadoraappk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonCalcular = btnCalcular
+
+        buttonCalcular.setOnClickListener{
+            calcularPreco(it)
+        }
+    }
+
+    fun calcularPreco(view: View){
+
+        val precoAlcool = txtAlcool.text.toString()
+        val precoGasolina = txtGasolina.text.toString()
+
+        val validaCampos = validarCampos(precoAlcool, precoGasolina)
+        if (validaCampos){
+            calcularMelhorPreco(precoAlcool, precoGasolina)
+        } else {
+            txtResultado.text = "Preencha valores de combustível válidos!!!"
+        }
+
+    }
+
+    private fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
+
+        var valorAlcool = precoAlcool.toDouble()
+        var valorGasolina = precoGasolina.toDouble()
+
+        val resultadoPreco = valorAlcool / valorGasolina
+
+        if (resultadoPreco >= 0.7){
+            txtResultado.text = "Melhor utilizar Gasolina!"
+        } else {
+            txtResultado.text = "Melhor utilizar Álcool!"
+        }
+
+
+    }
+
+    private fun validarCampos(precoAlcool: String, precoGasolina: String): Boolean {
+        var camposValidados: Boolean = true
+
+        if (precoAlcool == null || precoAlcool.equals("")){
+            camposValidados = false
+        } else if (precoGasolina == null || precoGasolina.equals("")){
+            camposValidados = false
+        }
+        return camposValidados
     }
 }
